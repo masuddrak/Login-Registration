348f2248625a8f3f509e3961027ee15ebb1f50b8
masud rana <masudranaacb@gmail.com>
2022-04-07 16:23:35 +0600

    using form control

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth } from "firebase/auth";
+import app from './firebase.init';
 
+const auth = getAuth(app);
 function App() {
+  const emailHandel=(event)=>{
+    console.log(event.target.value)
+  }
+  
+  const passworHandel=(event)=>{
+    console.log(event.target.value)
+  }
+  const submitHandel=(event)=>{
+    console.log('fgfh')
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Firebase authentication</h1>
+      <form onSubmit={submitHandel}  action="">
+        <input onBlur={emailHandel} type="text" />
+        <br></br>
+        <br></br>
+        <input onBlur={passworHandel} type="password" name="" id="" />
+        <br></br>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
